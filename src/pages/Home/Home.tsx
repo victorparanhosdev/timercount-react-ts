8b9314b2f26f725a4972160e39180d5f140eb95c
Victor Paranhos <victor_paranhos@hotmail.com>
2023-11-29 15:03:50 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 
-import { useState, useEffect} from 'react';
-import {Container} from './style'
-import {  differenceInSeconds} from 'date-fns'
+import { useState, useEffect } from 'react';
+import { Container } from './style'
+import { differenceInSeconds } from 'date-fns'
 
-import { Plus} from "@phosphor-icons/react"
-import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
+import { Plus } from "@phosphor-icons/react"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
 
 type newCicleProps = zod.infer<typeof newCicle>
@@ -18,89 +18,94 @@ const newCicle = zod.object({
   numeroplay: zod.number().min(1, 'minimo 1').max(60, 'o maximo é 60')
 })
 
-export function Home(){
-    const [dataTime, setdataTime] = useState<PropsTimer[]>([])
-    const [isActiveId, setActiveId] = useState<string | null>(null)
-    const [amouteSecondsPassed, setAmouteSecondsPassed] = useState(0)
-    const {register, watch, reset, handleSubmit, formState: {errors}} = useForm<newCicleProps>({
-      resolver: zodResolver(newCicle),
-      defaultValues: {
-        numeroplay: 0
-      }
-    })
+export function Home() {
+  const [dataTime, setdataTime] = useState<PropsTimer[]>([])
+  const [isActiveId, setActiveId] = useState<string | null>(null)
+  const [amouteSecondsPassed, setAmouteSecondsPassed] = useState(0)
 
-    function addDataForm(data: newCicleProps){
-      const DadosTimer = {
-        id: String(new Date().getTime()),
-        numeroplay: data.numeroplay,
-        startNumber: new Date(),
-      }
+  const { register, watch, reset, handleSubmit, formState: { errors } } = useForm<newCicleProps>({
+    resolver: zodResolver(newCicle),
+    defaultValues: {
+      numeroplay: 0
+    }
+  })
 
-      setActiveId(DadosTimer.id)
-      setAmouteSecondsPassed(0)
-      setdataTime(state=> [...state, DadosTimer])
-      reset()
+  function addDataForm(data: newCicleProps) {
+    const DadosTimer = {
+      id: String(new Date().getTime()),
+      numeroplay: data.numeroplay,
+      startNumber: new Date(),
     }
 
-    const isSubmitDisabled = watch('numeroplay')
-    const ActiveNumber = dataTime.find(dadostimer => dadostimer.id === isActiveId)
-    const dataSeconds = ActiveNumber ? ActiveNumber.numeroplay * 60 : 0
-    const currentSeconds = ActiveNumber? dataSeconds - amouteSecondsPassed : 0
-   
-    const minutesAmouts = Math.floor(currentSeconds / 60)
-    const secondsAmout = currentSeconds % 60
+    setActiveId(DadosTimer.id)
+    setAmouteSecondsPassed(0)
+    setdataTime(state => [...state, DadosTimer])
+    reset()
+  }
+
+  const isSubmitDisabled = watch('numeroplay')
+  const ActiveNumber = dataTime.find(dadostimer => dadostimer.id === isActiveId)
+
+  const dataSeconds = ActiveNumber ? ActiveNumber.numeroplay * 60 : 0
+  const currentSeconds = ActiveNumber ? dataSeconds - amouteSecondsPassed : 0
+
+  const minutesAmouts = Math.floor(currentSeconds / 60)
+  const secondsAmout = currentSeconds % 60
 
 
-    const minutes = String(minutesAmouts).padStart(2, '0')
-    const seconds = String(secondsAmout).padStart(2, '0')
+  const minutes = String(minutesAmouts).padStart(2, '0')
+  const seconds = String(secondsAmout).padStart(2, '0')
 
 
-    useEffect(()=> {
+  useEffect(() => {
 
-      let interval:number
 
-      if(ActiveNumber){
+    let interval: number
 
-        interval = setInterval(()=> {
+    if (ActiveNumber) {
+
+      interval = setInterval(() => {
+
         const CounterSeconds = differenceInSeconds(new Date(), ActiveNumber.startNumber)
-          setAmouteSecondsPassed(CounterSeconds)
-          if(CounterSeconds === dataSeconds){
-            setActiveId(null)
-          }
-          
+        setAmouteSecondsPassed(CounterSeconds)
+        if (CounterSeconds === dataSeconds) {
+          setActiveId(null)
 
-        }, 1000)
-     
-      }
-
-      return ()=>{
-
-        clearInterval(interval)
- 
-      }
-    },[ActiveNumber])
-
-   
+        }
 
 
-    
-    return(
-      <Container>
-        <form onSubmit={handleSubmit(addDataForm)}>
-          <div>
-            <label htmlFor="box-number">Cronometro</label>
-            <input min={0} type="number" id="box-number" {...register("numeroplay", {valueAsNumber: true})}/>
-          </div>
-          {errors.numeroplay && <p>{errors.numeroplay.message}</p>}
-          <button disabled={!isSubmitDisabled || isSubmitDisabled < 1} type="submit"><Plus size={24}/>  Adicionar</button>
+      }, 1000)
 
-          </form>
+    }
 
+    return () => {
+
+      clearInterval(interval)
+
+    }
+  }, [ActiveNumber])
+
+
+
+
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit(addDataForm)}>
         <div>
-            <span>{minutes}</span>
-            <span>:</span>
-            <span>{seconds}</span>
-          </div>
-      </Container>
-    )
+          <label htmlFor="box-number">Cronometro</label>
+          <input min={0} type="number" id="box-number" {...register("numeroplay", { valueAsNumber: true })} />
+        </div>
+        {errors.numeroplay && <p>{errors.numeroplay.message}</p>}
+        <button disabled={!isSubmitDisabled || isSubmitDisabled < 1} type="submit"><Plus size={24} />  Adicionar</button>
+
+      </form>
+
+      <div>
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+    </Container>
+  )
 }
